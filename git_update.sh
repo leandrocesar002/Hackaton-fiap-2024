dd0d48593271977f99a09f2b8c36f1c9e09ff440
leandro <leandrocmsantos@gmail.com>
2024-07-25 16:04:41 -0300

    script

--- git_update.sh
+++ git_update.sh
@@ -1,65 +1,33 @@
 #!/bin/bash
 
-VERSION=""
-
-# get parameters
-while getopts v: flag
-do
-  case "${flag}" in
-    v) VERSION=${OPTARG};;
-  esac
-done
-
-# get highest tag number, and add v0.1.0 if doesn't exist
-git fetch --prune --unshallow 2>/dev/null
-CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
-
-if [[ $CURRENT_VERSION == '' ]]
-then
-  CURRENT_VERSION='v0.1.0'
-fi
-echo "Current Version: $CURRENT_VERSION"
-
-# replace . with space so can split into an array
-CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./ })
-
-# get number parts
-VNUM1=${CURRENT_VERSION_PARTS[0]}
-VNUM2=${CURRENT_VERSION_PARTS[1]}
-VNUM3=${CURRENT_VERSION_PARTS[2]}
-
-if [[ $VERSION == 'major' ]]
-then
-  VNUM1=v$((VNUM1+1))
-elif [[ $VERSION == 'minor' ]]
-then
-  VNUM2=$((VNUM2+1))
-elif [[ $VERSION == 'patch' ]]
-then
-  VNUM3=$((VNUM3+1))
+# Verifique a versão passada
+if [ "$1" == "-v" ] && [ -n "$2" ]; then
+  VERSION_TYPE=$2
 else
-  echo "No version type (https://semver.org/) or incorrect type specified, try: -v [major, minor, patch]"
+  echo "Usage: $0 -v [major|minor|patch]"
   exit 1
 fi
 
-# create new tag
-NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
-echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
+# Obtenha a última tag do Git
+LAST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 
-# get current hash and see if it already has a tag
-GIT_COMMIT=`git rev-parse HEAD`
-NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
+# Incrementa a versão
+IFS='.' read -r -a VERSION_PARTS <<< "$LAST_TAG"
 
-# only tag if no tag already
-if [ -z "$NEEDS_TAG" ]; then
-  echo "Tagged with $NEW_TAG"
-  git tag $NEW_TAG
-  git push --tags
-  git push
+if [ "$VERSION_TYPE" == "major" ]; then
+  NEW_TAG="$((VERSION_PARTS[0]+1)).0.0"
+elif [ "$VERSION_TYPE" == "minor" ]; then
+  NEW_TAG="${VERSION_PARTS[0]}.$((VERSION_PARTS[1]+1)).0"
+elif [ "$VERSION_TYPE" == "patch" ]; then
+  NEW_TAG="${VERSION_PARTS[0]}.${VERSION_PARTS[1]}.$((VERSION_PARTS[2]+1))"
 else
-  echo "Already a tag on this commit"
+  echo "Invalid version type. Use major, minor, or patch."
+  exit 1
 fi
 
-echo ::set-output name=git-tag::$NEW_TAG
+# Crie uma nova tag
+git tag $NEW_TAG
+git push origin $NEW_TAG
 
-exit 0
+# Retorna a nova tag
+echo $NEW_TAG
